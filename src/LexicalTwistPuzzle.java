39a587f4132266643c317641392318c31604334e
komal624 <kb4858@srmist.edu.in>
2026-02-20 11:11:14 +0530

    [Komal] added : UC2 accept word inputs

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
 
     public static void main(String[] args) {
+
+        // UC1
         System.out.println("Lexical Twist Puzzle");
+
+        // UC2
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first word: ");
+        String word1 = scanner.nextLine();
+
+        System.out.print("Enter second word: ");
+        String word2 = scanner.nextLine();
     }
 }
